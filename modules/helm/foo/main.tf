58a451131dfba8ee7ec0031a8dcf81ef6acf406a
josh keife <jkeife@gmail.com>
2021-02-08 16:51:58 -0700

    helm module

---
+++ modules/helm/foo/main.tf
@@ -0,0 +1,11 @@
+resource "kubernetes_namespace" "demo" {
+  metadata {
+    name = "demo"
+  }
+}
+
+resource "helm_release" "hello-world" {
+  name = "hello-world"
+  chart = "${path.module}/chart"
+  namespace = kubernetes_namespace.demo.metadata.0.name
+}
