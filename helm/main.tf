c276a3f71b29b00acb3fc080a576644e7318e039
josh keife <jkeife@gmail.com>
2021-02-11 15:00:32 -0700

    update helm demo.

--- helm/main.tf
+++ helm/main.tf
@@ -1,9 +1,22 @@
+resource "kubernetes_namespace" "kube-state" {
+  metadata {
+    name = "kube-state-metrics"
+  }
+}
+
 resource "helm_release" "kube-state-metrics" {
   name = "kube-state-metrics"
 
   repository = "https://kubernetes.github.io/kube-state-metrics"
   chart      = "kube-state-metrics"
+  namespace  = kubernetes_namespace.kube-state.metadata.0.name
 
+}
+
+resource "kubernetes_namespace" "prom" {
+  metadata {
+    name = "prometheus"
+  }
 }
 
 resource "helm_release" "prometheus" {
@@ -11,12 +24,17 @@ resource "helm_release" "prometheus" {
 
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
+  namespace  = kubernetes_namespace.prom.metadata.0.name
 
   depends_on = [helm_release.kube-state-metrics]
-
 }
 
-
 module "foo" {
-  source = "git::https://github.com/CuddlyDemos/terraform-sandbox.git//modules/hello_world?ref=helm/foo/v1.0.0"
+  source    = "git::https://github.com/CuddlyDemos/terraform-sandbox.git//modules/helm/foo?ref=helm/foo/v1.0.1"
+  namespace = "foo"
+}
+
+module "bar" {
+  source    = "git::https://github.com/CuddlyDemos/terraform-sandbox.git//modules/helm/foo?ref=helm/foo/v1.0.1"
+  namespace = "bar"
 }
